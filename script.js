9758cbc23d9d408db9bf31114646b3a9ff381987
andreab <andreabqtmbc@gmail.com>
2016-03-28 10:25:38 -0500

    Calculadora del tiempo y agrgamos horas vividas

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ var segundosPorMinuto = 60;
 var minutosPorHora = 60;
 var horasPorDia = 24;
 var segundosPorDia = segundosPorMinuto * minutosPorHora * horasPorDia;
+document.write("<h1>Holis =)</h1>");
 document.write("<h1>Calculadora del Tiempo</h1><p>Hay " +segundosPorDia	+ " segundos en un dia</p>");
-/*var anios= prompt("Ingrese su edad");
-anVividos=anios*horasPorDia;
-document.write("<h1>Calculadora del Tiempo</h1><p>Has vividos " +anVividos	+ "  dias aproximadamente</p>");
-*/
-/*parseInt es una palabra reservada que convierte una acdena de caracteres en numeros*/
-var peliculasVistas = prompt("Cuantas peliculas haz visto este mes?");
-var seriesVistas = prompt("Cuantas series has visto este mes?");
-var tiempoPantalla = parseInt(peliculasVistas) + parseInt(seriesVistas);
-document.write('Wow! Has visto ' + tiempoPantalla + " veces minimo la pantalla.");
+
+// Saber cuantos horas has vivido
+var horaAnio= parseInt(horasPorDia)*365;
+var anios= prompt("Ingrese su edad");
+anVividos=anios*horaAnio;
+document.write("<h1>Calculadora del Tiempo</h1><p>Has vividos " +anVividos	+ "  horas aproximadamente</p>");
+
 
